6b6f7b0209b7eb5e60b07273e2e4baa044bfa173
mitchelkovacs <mdk181@mail.usask.ca>
2021-12-27 00:32:01 +0000

    in process of testing checkPermutation

--- Chapter1.py
+++ Chapter1.py
@@ -9,10 +9,31 @@ def isUnique(string):
 			if string[i] == string[n]:
 				return False
 			n = n+1
-	return True		
+	return True	
 
-#CHAPTER 1 TESTING
-SELECT = input("Choose a function for testing: \n" + "1. isUnique\n \n")
+#1.2 Given two strings, check if one is a permutation of the other
+def checkPermutation(string1,string2):
+	if len(string1) != len(string2):
+		return False
+	else:
+		for i in string1: 
+			for n in string2:
+				if i == n:
+					#same characters so remove them both and keep looking
+					string1 = string1.replace(i, "",1)
+					string2 = string2.replace(n, "",1)
+					print(string1 + "\n" + string2)
+					break
+		#if it is a permutation both strings have had all characters removed and should have length of 0			
+		if string1 == string2:
+			return True
+		else:
+			return False
+
+
+#CHAPTER 1 TESTING - chose to take input so others could test
+SELECT = input("Choose a function for testing: \n" + "1. isUnique\n" + "2. checkPermutation\n \n")
+
 
 #1.1
 if SELECT == '1':
@@ -23,3 +44,13 @@ if SELECT == '1':
 		print("\nunique\n")
 	else:
 		print("\nnot unique\n")
+
+#1.2
+elif SELECT == '2':
+
+	t1, t2= input("\nTesting for checkPermutation. \nEnter two strings: ").split()
+	
+	if checkPermutation(t1, t2):
+		print("\nis permutation\n")
+	else:
+		print("\nis not permutation\n")
