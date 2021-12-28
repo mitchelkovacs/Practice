81f6faebe59a7303e28cc117321756518755a9ee
mitchelkovacs <mdk181@mail.usask.ca>
2021-12-28 02:04:15 +0000

    finished and tested URLify and finished and tested permuation palindrome

--- Chapter1.py
+++ Chapter1.py
@@ -1,5 +1,7 @@
 #Mitchel Kovacs
 #Practice questions from "Cracking the Coding Interview"
+#Chapter 1 - Arrays and Strings
+
 
 #1.1 Determine if a String has all unique characters without additional data structures
 def isUnique(string):
@@ -22,7 +24,7 @@ def checkPermutation(string1,string2):
 					#same characters so remove them both and keep looking
 					string1 = string1.replace(i, "",1)
 					string2 = string2.replace(n, "",1)
-					print(string1 + "\n" + string2)
+
 					break
 		#if it is a permutation both strings have had all characters removed and should have length of 0			
 		if string1 == string2:
@@ -30,9 +32,47 @@ def checkPermutation(string1,string2):
 		else:
 			return False
 
+#1.3 replace all spaces in a string with '%20'
+def URLify(string):
+	return string.replace(' ', "%20")
+
+#1.4 check if a string is a permutation of a palindrome
+def palindromePermutaion(string):
+	string = string.replace(" ", "")
+	string = string.lower()
+	isOdd = len(string) % 2
+	i = 0
+	while i < len(string): 
+		n = i + 1
+		while n < len(string):
+			if string[i] == string[n]:
+				#same characters so remove them both and keep looking
+				string = string.replace(string[i], "",2)
+				i = i - 1
+				break
+			n = n + 1
+		i = i + 1
+	#not sure if its needed to check if odd but in my mind it makes sense	
+	if isOdd == 1:
+		if len(string) == 1:
+			return True
+		else:
+			return False
+	else:
+		if len(string) == 0:
+			return True
+		else:
+			return False
+
+
+
 
 #CHAPTER 1 TESTING - chose to take input so others could test
-SELECT = input("Choose a function for testing: \n" + "1. isUnique\n" + "2. checkPermutation\n \n")
+SELECT = input("Choose a function for testing: \n" + "1. isUnique\n"
+	 + "2. checkPermutation\n"
+	 + "3. URLify\n"
+ 	 + "4. palindromePermutation\n"
+	 + "\n")
 
 
 #1.1
@@ -54,3 +94,20 @@ elif SELECT == '2':
 		print("\nis permutation\n")
 	else:
 		print("\nis not permutation\n")
+
+#1.3
+elif SELECT == '3':
+
+	t1 = input("\nTesting for URLify. \nEnter a string: ")
+	
+	print("\n" + URLify(t1)+ "\n")
+
+#1.4
+elif SELECT == '4':
+
+	t1 = input("\nTesting for palindromePermutaion. \nEnter a string: ")
+
+	if palindromePermutaion(t1):
+		print("\nis a permutation of a palindrome\n")
+	else:
+		print("\nis not a permutation of a palindrome\n")
